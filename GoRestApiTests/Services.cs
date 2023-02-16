4327c1b155702333fca036a7c590abd0d57475cd
RumyanaYocnheva <RYoncheva@yahoo.com>
2023-02-16 11:30:09 +0200

    Added Services

---
+++ GoRestApiTests/Services.cs
@@ -0,0 +1,12 @@
+﻿using GoRestApiTests.Support;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Services
+{
+    public void ConfigureServices()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<HttpClientService>();
+        services.AddSingleton<Config>();        
+    }
+}
---
+++ GoRestApiTests/Support/TestContext.cs
@@ -0,0 +1,12 @@
+﻿using GoRestTests.Models;
+
+namespace GoRestTests.Support
+{
+    public class TestContext
+    {
+        public string BaseUrl { get; set; }
+
+        public GetUsersModel GetRequestResult { get; set; }
+
+    }
+}
